d82a03977a7484da42a7ee6495a5ec059067fa4b
rob <rob@angle.fish>
2020-07-15 12:06:55 -0700

    added music to planck

--- keyboards/planck/keymaps/maclike/config.h
+++ keyboards/planck/keymaps/maclike/config.h
@@ -39,3 +39,5 @@
 
 
 #define TAPPING_TERM 200
+
+#define AUDIO_CLICKY
--- keyboards/planck/keymaps/maclike/keymap.c
+++ keyboards/planck/keymaps/maclike/keymap.c
@@ -14,6 +14,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+// Compile: qmk compile -kb planck/rev6 -km maclike
+
 #include QMK_KEYBOARD_H
 #include "muse.h"
 #include "quantum.h"
@@ -22,12 +24,16 @@
 enum planck_layers {
   DefaultLayer,
   NumpadLayer,
-  FnLayer
+  FnLayer,
+  Fn1Layer,
 };
 
 #define RAISE MO(NumpadLayer)
+#define RAISE1 MO(Fn1Layer)
 #define ESC_FN LT(FnLayer, KC_ESC)
 
+#define MACBRID KC_F14
+#define MACBRIU KC_F15
 
 enum {
   TD_COLQUOT = 0
@@ -47,60 +53,67 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  Up  |Shift |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | x    | Ctrl | Alt  | CMD  |RAISE |    Space    |RAISE | Cmd  | Left | Down |Right |
+ * | Ctrl | Alt  | CMD  |      |RAISE |    Space    |RAISE | Cmd  | Left | Down |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [DefaultLayer] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     ESC_FN,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    COLQUOT, KC_ENT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_RSFT,
-    XXXXXXX, KC_LCTL, KC_LALT, KC_LCMD, RAISE,   KC_SPC,  KC_SPC,  RAISE,   KC_RCMD, KC_LEFT, KC_DOWN, KC_RGHT
+    KC_LCTL, KC_LALT, KC_LCMD, XXXXXXX, RAISE,   KC_SPC,  KC_SPC,  RAISE,   KC_RCMD, KC_LEFT, KC_DOWN, KC_RGHT
 ),
 
 /* Numpad and Symbols
  * ,-----------------------------------------------------------------------------------.
- * |  `   |   1  |   2  |   3  |   \  |   !  |   |  |   (  |   )  |   -  |   +  |  =   |
+ * |  `   |   1  |   2  |   3  |   \  |   !  |   |  |   (  |   )  |   -  |   +  | [v]  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | [v]  |   4  |   5  |   6  |      |   ?  |   &  |   {  |   }  |   /  |   *  |  =   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | [v]  |   7  |   8  |   9  |   0  |      |      |   [  |   ]  |   .  | [v]  | [v]  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | [v]  | [v]  | [v]  | [v]  | [v]  |     [v]     | [v]  | [v]  | [v]  | [v]  | [v]  |
+ * | [v]  | [v]  | [v]  | [v]  | Fn1  |     [v]     | Fn1  | [v]  | [v]  | [v]  | [v]  |
  * `-----------------------------------------------------------------------------------'
  */
 [NumpadLayer] = LAYOUT_planck_grid(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_BSLS, KC_EXLM, KC_PIPE, KC_LPRN, KC_RPRN, KC_MINS, KC_PPLS, KC_DEL,
-    _______, KC_4,    KC_5,    KC_6,    XXXXXXX, KC_QUES, KC_AMPR, KC_LCBR, KC_RCBR, KC_PSLS, KC_PAST, _______,
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_BSLS, KC_EXLM, KC_PIPE, KC_LPRN, KC_RPRN, KC_MINS, KC_PPLS, _______,
+    _______, KC_4,    KC_5,    KC_6,    XXXXXXX, KC_QUES, KC_AMPR, KC_LCBR, KC_RCBR, KC_PSLS, KC_PAST, KC_PEQL,
     _______, KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    _______, _______, _______, _______, RAISE1,  _______, _______, RAISE1,  _______, _______, _______, _______
 ),
 
 /* Fn
  * ,-----------------------------------------------------------------------------------.
- * | [v]  |  F1  |  F2  |  F3  | Reset|      |      |      |      |      |      | Del  |
+ * |  `   |  F1  |  F2  |  F3  | Reset|      |      |      |      |      |      | Del  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | [v]  |  F4  |  F5  |  F6  |      |      |      | Prev | Play | Next |      |WheelD|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | [v]  |  F7  |  F8  |  F9  |      | ScBr-| ScBr+| Mute | Vol- | Vol+ |MouseU|WheelU|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | [v]  | [v]  | [v]  | [v]  | [v]  |   Mouse 1   | [v]  | [v]  |MouseL|MouseD|MouseR|
+ * | [v]  | [v]  | CMD  | [v]  | [v]  |   Mouse 1   | [v]  | [v]  |MouseL|MouseD|MouseR|
  * `-----------------------------------------------------------------------------------'
  */
 [FnLayer] = LAYOUT_planck_grid(
-    _______, KC_F1,   KC_F2,   KC_F3,   RESET,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_DEL,
+    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   RESET,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_DEL,
     _______, KC_F4,   KC_F5,   KC_F6,   KC_F10,  KC_F11,  KC_F12,  KC_MRWD, KC_MPLY, KC_MFFD, XXXXXXX, KC_WH_D,
-    _______, KC_F7,   KC_F8,   KC_F9,   XXXXXXX, KC_BRID, KC_BRIU, KC_MUTE, KC_VOLD, KC_VOLU, KC_MS_U, KC_WH_U,
-    _______, _______, _______, _______, _______, KC_BTN1, KC_BTN1, _______, _______, KC_MS_L, KC_MS_D, KC_MS_R
+    _______, KC_F7,   KC_F8,   KC_F9,   XXXXXXX, MACBRID, MACBRIU, KC_MUTE, KC_VOLD, KC_VOLU, KC_MS_U, KC_WH_U,
+    _______, _______, KC_LCMD, _______, _______, KC_BTN1, KC_BTN1, _______, _______, KC_MS_L, KC_MS_D, KC_MS_R
+),
+
+[Fn1Layer] = LAYOUT_planck_grid(
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   RESET, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, CK_TOGG, XXXXXXX, XXXXXXX,  MU_MOD,  MU_TOG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 ),
 
 };
 
-/*#ifdef AUDIO_ENABLE
+#ifdef AUDIO_ENABLE
   float plover_song[][2]     = SONG(PLOVER_SOUND);
   float plover_gb_song[][2]  = SONG(PLOVER_GOODBYE_SOUND);
 #endif
 
-layer_state_t layer_state_set_user(layer_state_t state) {
+/*layer_state_t layer_state_set_user(layer_state_t state) {
   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
 
